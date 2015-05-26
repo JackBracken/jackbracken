b93f409cd4eb99c1f5ac79467bc508329862ca01
Jack Bracken <contact@jacktrick.com>
2015-05-26 16:58:02 +0100

    Restored config dir to version control

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'home#index'
+
+  get '/cv', to: 'static#cv', as: 'cv_page'
+  get '/contact', to: 'static#contact', as: 'contact_page'
+
+  resources :photograph, path: :'photography', only: [:index, :new, :destroy, :create, :show, :edit, :update]
+end
