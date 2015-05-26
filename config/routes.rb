3a76a3e073a902d8ebfdd4785ea612e8fd0bd630
Jack Bracken <contact@jacktrick.com>
2015-05-26 17:50:52 +0100

    Begin work on Contact Mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +7,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/cv', to: 'static#cv', as: 'cv_page'
-  get '/contact', to: 'static#contact', as: 'contact_page'
+  # get '/contact', to: 'static#contact', as: 'contact_page'
 
   resources :photograph, path: :'photography', only: [:index, :new, :destroy, :create, :show, :edit, :update]
+  match '/contact', to: 'contact_pages#new', via: 'get'
+  resources :contact_pages, path: :'contact', only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427112804) do
+ActiveRecord::Schema.define(version: 20150526160253) do
+
+  create_table "contact_pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "from"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
